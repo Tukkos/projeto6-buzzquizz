a4f2f27fdd2299e3f92692739d30cea78dc47621
Tukkos <jpfcastro@id.uff.br>
2022-07-01 12:42:55 -0300

    Tela 2 ~quase finalizada

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const urlQuizzUnico = "https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes/1
 
 let pontos = 0;
 let perguntasRespondidas = 0;
+let perguntas = 0;
 let respostas = [];
+let renderQuizbody = ``;
 
 buscarQuizz();
 
@@ -18,51 +20,45 @@ function mix() {
 }
 
 function renderizarMensagens(dados) {
-    console.log(dados);
+    // console.log(dados);
     let quizBody = document.querySelector(".quizBody");
-    let renderQuizbody = ``;
+    renderQuizbody = ``;
     const axios = dados.data;
 
-    // for (let k=0; k<axios.questions[i].answers.length; k++) {
-    //     respostas = [questions[k]: [
-
-    //     ]
-
-    //     ]
-    // }
-
-
     renderQuizbody += `
-    <div class="bannerQuizz" background: linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${dados.data.image});>
-        <h1 class="tituloQuizz">${dados.data.title}</h1>
-        <img class="topoSegundaTela" src="${dados.data.image}" alt="">
+    <div class="bannerQuizz" background: linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${axios.image});>
+        <h1 class="tituloQuizz">${axios.title}</h1>
+        <img class="topoSegundaTela" src="${axios.image}" alt="">
     </div>
     `;
 
     for (let i = 0; i < axios.questions.length; i++) {
+        perguntas = axios.questions.length;
+        // console.log(perguntas);
+
         renderQuizbody += `
         <div class="pergunta">
-            <div style="background-color: ${dados.data.questions[i].color};" class="cxPergunta">
-                <p>${dados.data.questions[i].title}</p >
+            <div style="background-color: ${axios.questions[i].color};" class="cxPergunta">
+                <p>${axios.questions[i].title}</p >
             </div >
         </div >
             <div class="respostas respostaEscondida respostaOnHold proximo">
                 `;
 
-        dados.data.questions[i].answers.sort(mix);
+        axios.questions[i].answers.sort(mix);
 
         for (let j = 0; j < axios.questions[i].answers.length; j++) {
-            if (dados.data.questions[i].answers[j].isCorrectAnswer === true) {
+            if (axios.questions[i].answers[j].isCorrectAnswer === true) {
                 renderQuizbody += `
             <div class="respostaCerta" onclick="selecionarResposta(this)">
-                <img class="imgPerg" src="${dados.data.questions[i].answers[j].image}" alt="">
-                <p>${dados.data.questions[i].answers[j].text}</p>
+                <img class="imgPerg" src="${axios.questions[i].answers[j].image}" alt="">
+                <p>${axios.questions[i].answers[j].text}</p>
             </div>`;
             } else {
                 renderQuizbody += `
             <div class="respostaErrada" onclick="selecionarResposta(this)">
-                <img class="imgPerg" src="${dados.data.questions[i].answers[j].image}" alt="">
-                <p>${dados.data.questions[i].answers[j].text}</p>
+                <img class="imgPerg" src="${axios.questions[i].answers[j].image}" alt="">
+                <p>${axios.questions[i].answers[j].text}</p>
             </div>`;
             }
         }
@@ -89,7 +85,9 @@ function selecionarResposta(elemento) {
         resp.classList.remove(`proximo`);
         perguntasRespondidas++
 
-        // if (perguntasRespondidas === )
+        if (perguntasRespondidas === perguntas) {
+            buscarQuizz2();
+        }
         setTimeout(scroll, 2000);
     }
 }
@@ -97,4 +95,41 @@ function selecionarResposta(elemento) {
 function scroll() {
     document.querySelector(`.proximo`).scrollIntoView({ behavior: `smooth`, block: `end` });
 }
+
+function buscarQuizz2() {
+    const promessa = axios.get(urlQuizzUnico);
+    console.log(promessa);
+    promessa.then(renderizarResultado);
+}
+
+function renderizarResultado(dados) {
+    console.log(dados);
+    let quizBody = document.querySelector(".quizBody");
+    const axios = dados.data;
+
+    let porcentagem = Math.round(pontos / perguntas * 100);
+
+    for (let k = 0; k < axios.levels.length; k++) {
+
+        if (porcentagem >= axios.levels[k].minValue) {
+            quizBody.innerHTML += `
+            <div class="pergunta">
+                <div style="background-color: #EC362D;" class="cxPergunta">
+                    <p>${porcentagem}% de acerto: ${axios.levels[k].title}</p>
+                </div>
+            </div>
+            <div class="resultadoAqui">
+                <img class="imgResult" src="${axios.levels[k].image}" alt="">
+                <p>${axios.levels[k].text}</p>
+            </div>
+        
+            <button class="botaoVermelho" onclick="acessarQuizz()">Acessar Quizz</button>
+            <div></div>
+            <button class="botaoBranco" onclick="voltarHome()">Voltar pra home</button>
+            `;
+            return
+        }
+    }
+}
+
 
